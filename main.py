e1e903d4385a07a454127cf616425abddc3e20e9
Sastiazaran <sastiazaran2000@gmail.com>
2023-04-27 00:56:08 -0600

    addition of the features of levels of RGB color and also the calculus of the histogram

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 import csv
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 folders=["attackOnTitan_fotos", "deathNote_fotos", "Evangelion", "KimetsuNoYaiba", "LOTR","NBA", "onePiece_fotos", "SNKRS", "StarWars"]
 names =["AOT (","deathNote (","Evangel (","KNY (","LOTR (", "nba (","OnePiece (", "Snkrs (", "SW ("]
+
 
 # Función para calcular las características de una imagen
 def extract_features(img, name, folder):
@@ -13,13 +16,31 @@ def extract_features(img, name, folder):
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(gray)
     edges = cv2.Canny(gray, 50, 150)
     n_edges = cv2.countNonZero(edges)
-    return [name,folder, mean, std_dev, min_val, max_val, n_edges]
+
+    #saturation of the red channel
+    hsvR = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    satR = cv2.mean(hsvR[:,:,0])[0]
+    histR = cv2.calcHist([img], [2], None, [256], [0, 256])
+
+
+    #saturation of the green channel
+    hsvG = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    satG = cv2.mean(hsvG[:,:,1])[0]
+    histG = cv2.calcHist([img], [1], None, [256], [0, 256])
+
+    #saturation of the blue channel
+    hsvB = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    satB = cv2.mean(hsvB[:,:,2])[0]
+    histB = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+    return [name,folder, mean, std_dev, min_val, max_val, n_edges, satR, satG, satB, histR, histG, histB]
 
 
 with open('features.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
 
-    writer.writerow(['Archivo','Name','Folder', 'Media', 'Desviacion estandar', 'Valor minimo', 'Valor maximo', 'Numero de bordes'])
+    writer.writerow(['Archivo','Name','Folder', 'Media', 'Desviacion estandar', 'Valor minimo', 'Valor maximo', 'Numero de bordes', 'Saturación de rojo',
+                     'Saturacion de verde', 'Saturacion de azul'])
 
     for j in range(9):
         for i in range(1, 101):
@@ -44,3 +65,15 @@ while True:
     nameOpc = names[carpeta-1]+id+').png'
     imgOpc = cv2.imread('./fotosGabriel/'+folders[carpeta-1]+'/'+name)
     print("Valores ", extract_features(img, nameOpc, folders[carpeta-1]))
+    # trazar el histograma del canal rojo
+    plt.plot(features[-3], color='r')
+    plt.xlim([0, 256])
+    plt.show()
+    # trazar el histograma del canal verde
+    plt.plot(features[-2], color='g')
+    plt.xlim([0, 256])
+    plt.show()
+    # trazar el histograma del canal azul
+    plt.plot(features[-1], color='b')
+    plt.xlim([0, 256])
+    plt.show()
